355f2a17115f6a54c3252728b1f62147fff5dab5
Marcin Bratek <bratekmar@gmail.com>
2022-05-05 12:56:34 +0200

    MainExtended

---
+++ kodilla-intro/src/main/java/MainExtended.java
@@ -0,0 +1,16 @@
+public class MainExtended {
+    public static void main(String[] args) {
+        String cannonballRecord = "Saab 9-5";
+        int timeH = 65;
+        int timeM = 28;
+        double averageSpeed =140;
+        char carSegment = 'E';
+        boolean coastToCoastAndBack = true;
+        System.out.println(cannonballRecord);
+        System.out.println(timeH);
+        System.out.println(timeM);
+        System.out.println(averageSpeed);
+        System.out.println(carSegment);
+        System.out.println(coastToCoastAndBack);
+    }
+}
